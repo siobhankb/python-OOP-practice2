78c214132c41fe3fff485754645561bc4fc41301
siobhankb <siobhan.boylan@gmail.com>
2022-05-17 17:32:00 -0500

    ready to turn in

--- example_module.py
+++
@@ -1,5 +0,0 @@
-def greet(name):
-    print(f"Hello, {name.title()}. How are you doing today?")
-
-def leave(name):
-    print(f"Goodbye, {name.title()}. It was nice seeing you...")
---
+++ geometry.py
@@ -0,0 +1,7 @@
+import math
+
+def circle_area(circle_r):
+    return math.pi * (circle_r**2)
+
+def hypotenuse(triangle_side_a, triangle_side_b):
+    return ((triangle_side_a**2) + (triangle_side_b**2))**(1/2)
